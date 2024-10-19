b1d319775b414d26dc88ad1bdf093eb3179866b8
decaljoonho <161907483+decaljoonho@users.noreply.github.com>
2024-10-19 11:49:47 +0900

    Add files via upload

---
+++ 241019.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main()
+//{
+//	int a = 20;
+//	
+//	a = a+10;
+//	printf("a = %d\n", a);
+//	a = a * 10;
+//	printf("a = %d\n", a);
+//	a = a- 10;
+//	printf("a = %d\n", a);
+//	a = a / 10;
+//	printf("a = %d\n",a);
+//	
+//}
+
+//{
+//	int a = 10;
+//	
+//	a++;
+//	a++;
+//	printf("a = %d\n", a);
+//	++a;
+//	++a;
+//	printf("a = %d\n",a);
+//	
+//
+//}
+
+//{
+//	int a= 20;
+//	
+//	a += 4;
+//	printf("a = %d\n", a);
+//	a /= 2;
+//	printf("a = %d\n", a);
+//	a -= 1;
+//	printf("a = %d\n", a);
+//	a *= 11;
+//	printf("a = %d\n", a);
+//	
+//}
+
+{
+	int a = 10;
+	int b = 2;
+	
+	a = a-b;
+	printf("a = %d\n", a);
+	a = a * b;
+	printf("a = %d\n", a);
+	a--;
+	printf("a = %d\n", a);
+	a = --b;
+	printf("a = %d b = %d\n", a,b);
+	a = ++++b;
+	printf("a = %d\n", a);
+	a = ++b;
+	printf("a = %d\n", a);
+	a = ----b;
+	printf("a = %d\n", a);
+	
+	
+
+}
