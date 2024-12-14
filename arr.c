018f111e5889f87e07844525e90e64ac0afb6a38
decaljoonho <161907483+decaljoonho@users.noreply.github.com>
2024-12-14 13:00:02 +0900

    Add files via upload

---
+++ arr.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int arr[3];
+	arr[0] = 3;
+	arr[1] = 2;
+	arr[2] = 5;
+	
+	printf("%d\n",arr[0]);
+	printf("%d\n",arr[1]);
+	printf("%d\n",arr[2]);
+	return 0;
+}
