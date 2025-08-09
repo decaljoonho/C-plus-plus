0c56ede6f109aeb224c77dc7c2ecfe270e095841
doryato <161907483+decaljoonho@users.noreply.github.com>
2025-08-09 11:50:10 +0900

    Add files via upload

---
+++ 250809.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <string>
+#include <iostream>
+#include <map>
+#include <stdio.h>
+
+using namespace std;
+
+//int a[1000001]={0,1};
+//int main()
+//{
+//	int M, N;
+//	cin>>M>>N;
+//
+//	for(int i=2; i<=N; i++)
+//		for(int j=2; i*j<=N; j++)
+//			a[i*j] = 1;
+//
+//	for(int i=M;i<=N;i++)
+//		if(!a[i])
+//			printf("%d\n",i);
+//   
+//	return 0;
+//}
+
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<n;
+}
+
+
