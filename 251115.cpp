5908977229d9752c93eabfd06855a6de8bd832df
doryato <161907483+decaljoonho@users.noreply.github.com>
2025-11-15 11:50:49 +0900

    Add files via upload

---
+++ 251115.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+#define MAX 9
+using namespace std;
+
+//int arr[8];
+//int N = 0;
+//bool visited[8];
+//int num[8];
+//
+//void dfs(int count)
+//{
+//	if(count == N)
+//	{
+//		for(int i=0; i<N; i++)
+//		{
+//			cout << arr[i] << ' ';
+//		}
+//		cout << '\n';
+//	}
+//	
+//	for(int i=0; i<N; i++)
+//	{
+//		if(visited[i])
+//		{
+//			continue;
+//		}
+//		visited[i] = true;
+//		arr[count] = i+1;
+//		dfs(count + 1);
+//		visited[i] = false;
+//	}
+//	
+//}
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(0);
+//	cout.tie(0);
+//	
+//	cin >> N;
+//	
+//	dfs(0);
+//	
+//	return 0;
+//	
+//}
+
+
+
+//int n,m;
+//int arr[MAX] = {0, };
+//bool visited[MAX] = {0, };
+//
+//void dfs(int count)
+//{
+//	if(count == m)
+//	{
+//		for(int i=0; i<m; i++)
+//			cout << arr[i] << ' ';
+//		cout << '\n';
+//	}
+//	
+//	for(int i=1; i<=n; i++)
+//		if(!visited[i])
+//		{
+//			visited[i] = true;
+//			arr[count] = i;
+//			dfs(count + 1);
+//			visited[i] = false;
+//		}
+//}
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(0);
+//	cout.tie(0);
+//	
+//	cin >> n >> m;
+//	
+//	dfs(0);
+//	return 0;
+//}
+
+
+void dfs()
+{
+	
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	
+	dfs(0);
+	return 0;
+}
